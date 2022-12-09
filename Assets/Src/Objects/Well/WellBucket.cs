0e3fa12fd72d6ad54759c0cbfe212742a0013f74
max <maxwippich@hotmail.com>
2022-12-09 13:24:57 +0100

    finished well interaction

--- Assets/Src/Objects/Well/WellBucket.cs
+++ Assets/Src/Objects/Well/WellBucket.cs
@@ -8,10 +8,19 @@ public class WellBucket : MonoBehaviour
     // Start is called before the first frame update
 
     public CircularDrive circularDrive;
+    public float minZ, maxZ;
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = transform.position + new Vector3(0, circularDrive.rotationSpeed, 0);
+        transform.localPosition += new Vector3(0, 0, circularDrive.rotationSpeed*100);
+        if(transform.localPosition.z < minZ)
+        {
+            transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y, minZ);
+        }
+        if (transform.localPosition.z > maxZ)
+        {
+            transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y, maxZ);
+        }
     }
 }
